96111eece13e9166e701a9812149005c567b5438
Kailey09 <kcoop1323@gmail.com>
2022-06-27 22:07:01 -0400

    updated routes to include comments

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -16,21 +16,30 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   User.findOne({
     attributes: { exclude: ['password'] },
-    where: {
-      id: req.params.id
+  where: {
+    id: req.params.id
+  },
+  include: [
+    {
+      model: Post,
+      attributes: ['id', 'title', 'post_url', 'created_at']
     },
-    include: [
-      {
+    // include the Comment model here:
+    {
+      model: Comment,
+      attributes: ['id', 'comment_text', 'created_at'],
+      include: {
         model: Post,
-        attributes: ['id', 'title', 'post_url', 'created_at'] 
-      },
-      {
-        model: Post,
-        attributes: ['title'],
-        through: Vote,
-        as: 'voted_posts'
+        attributes: ['title']
       }
-    ]
+    },
+    {
+      model: Post,
+      attributes: ['title'],
+      through: Vote,
+      as: 'voted_posts'
+    }
+  ]
   })
     .then(dbUserData => {
       if (!dbUserData) {
